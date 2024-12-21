705aa68641ee9f82e444d068c0d994b283c60d89
Akshay <akshay@olvy.co>
2024-12-21 19:34:29 +0530

    fix - data fetching issue with packaged extension

--- src/api/generateJSDoc.ts
+++ src/api/generateJSDoc.ts
@@ -1,23 +1,32 @@
-export async function getGeneratedJSDoc(functionCode: string): Promise<string> {
-  const API_URL = process.env.GENERATE_DOC_URL;
-  const ANON_KEY = process.env.SUPABASE_ANON_KEY;
+export async function getGeneratedJSDoc(
+  functionCode: string,
+  eid: string
+): Promise<string> {
+  // const API_URL = process.env.GENERATE_DOC_URL;
+  // const ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
-  if (!API_URL || !ANON_KEY) {
-    return Promise.reject(new Error("API URL & ANON_KEY are required."));
-  }
+  // if (!API_URL || !ANON_KEY) {
+  //   return Promise.reject(new Error("API URL & ANON_KEY are required."));
+  // }
 
   try {
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${ANON_KEY}`
-      },
-      body: JSON.stringify({ functionCode }),
-    });
-  
-    if(!response.ok) {
-      throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+    const response = await fetch(
+      "https://fwhzpfrjozntigdlpytn.supabase.co/functions/v1/generateJSDocComments",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-eid": `${eid}`,
+          // "Authorization": `Bearer ${ANON_KEY}`
+        },
+        body: JSON.stringify({ functionCode }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch: ${response.status} ${response.statusText}`
+      );
     }
     const generatedComment = await response.json();
     if (typeof generatedComment === "string") {
@@ -25,8 +34,8 @@ export async function getGeneratedJSDoc(functionCode: string): Promise<string> {
     } else {
       throw new Error("Unexpected response type: Expected a string.");
     }
-  } catch(error) {
+  } catch (error) {
     console.error("Error: ", error);
-    throw new Error('Error while generating jsdoc comments');
+    throw new Error("Error while generating jsdoc comments");
   }
 }
