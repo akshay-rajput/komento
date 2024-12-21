c262c79e607a32803e514ea9e8387c07e84a07b7
Akshay <akshay@olvy.co>
2024-12-21 20:19:13 +0530

    fix - remove eid config code

--- src/utils/extensionuid.ts
+++ src/utils/extensionuid.ts
@@ -5,18 +5,11 @@ import { v4 as uuidv4 } from "uuid";
 // Get or generate a unique UUID for the installation
 export async function getExtensionUUID(): Promise<string | null> {
   try {
-    const extensionUUID: string =
-      vscode.workspace.getConfiguration("komento").get("eid") || "";
-    if (!extensionUUID) {
-      const newUUID = uuidv4();
-      const randomName = dockerNames.getRandomName();
-      const uid = randomName + "_" + newUUID;
-      await vscode.workspace
-        .getConfiguration("komento")
-        .update("eid", uid, vscode.ConfigurationTarget.Global);
-      return uid;
-    }
-    return extensionUUID;
+    const newUUID = uuidv4();
+    const randomName = dockerNames.getRandomName();
+    const uid = randomName + "_" + newUUID;
+
+    return uid;
   } catch (e) {
     console.log("error during extuid: ", e);
     return null;
